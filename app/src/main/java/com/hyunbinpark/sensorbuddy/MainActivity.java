dad96cca5124e808f98c70d6a18f1314e41a69c4
hyunbin <hyunbip@gmail.com>
2016-02-03 19:22:59 -0600

    Accelometer readings, update dependencies

--- app/src/main/java/com/hyunbinpark/sensorbuddy/MainActivity.java
+++ app/src/main/java/com/hyunbinpark/sensorbuddy/MainActivity.java
@@ -1,15 +1,35 @@
 package com.hyunbinpark.sensorbuddy;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    private Button mStartButton;
+    private Button mStopButton;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccel;
+    private Sensor mGyro;
+    private Sensor mMag;
+    private Sensor mLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,50 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Initialize button logic
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerSensors();
+            }
+        });
+        mStopButton = (Button) findViewById(R.id.stop_button);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unregisterSensors();
+            }
+        });
+
+        // Initialize sensor-related objects
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    private void registerSensors(){
+        mSensorManager.registerListener(this, mAccel, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    private void unregisterSensors(){
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor == mAccel){
+            Log.d(TAG, Arrays.toString(event.values));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        Log.d(TAG, "Accuracy changed: " + sensor.getName() + " , " + i);
     }
 
     @Override
